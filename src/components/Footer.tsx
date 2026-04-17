29c4b5e194bc6d9b860b55772cda4ed3b9be4572
TosinISOGUN <oluwatomisinisogun@gmail.com>
2026-04-17 08:04:28 +0100

    feat: Social media links updates, TikTok now included

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -83,7 +83,7 @@ const Footer = () => {
                 >
                   <path d="M9 12a4 4 0 1 0 4 4V4a5 5 0 0 0 5 5" />
                 </svg>
-                TikTok
+                @01nachie
               </a>
             </div>
           </div>
